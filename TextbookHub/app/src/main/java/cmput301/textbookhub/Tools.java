51ef6e3fd1ef578301b6cbd3ff99274185bcc0c8
Jayden Chan <Jayden.t.chan@gmail.com>
2016-04-04 01:21:04 -0600

    Added Javadocs to Tools.java

--- TextbookHub/app/src/main/java/cmput301/textbookhub/Tools.java
+++ TextbookHub/app/src/main/java/cmput301/textbookhub/Tools.java
@@ -3,19 +3,44 @@ package cmput301.textbookhub;
 import android.graphics.Bitmap;
 
 /**
+ * <code>Tools</code> is used as a general class for methods that might be useful during programming.
+ *
+ * @author Thirteam
+ * @version 1.0
+ * @since 2016/03/12
+ *
  * Created by Fred on 2016/3/12.
  */
 public class Tools {
 
+    /**
+     * <code>isStringValid</code> checks if the <code>String</code> is empty or not
+     *
+     * @param s the <code>String</code> to be tested
+     * @return <code>true</code> if the <code>String</code> is valid and <code>false</code> otherwise.
+     */
     public static boolean isStringValid(String s){
         return (s != null && !s.isEmpty());
     }
 
+    /**
+     * <code>roundDecimal</code> rounds decimals
+     *
+     * @param place the place to round to (tenths, hundredths, etc)
+     * @param num the <code>Double</code> to round
+     * @return a <code>String</code> with the final rounded number.
+     */
     public static String roundDecimal(int place, Double num){
         String format_str = "%."+String.valueOf(place)+"f";
         return String.format(format_str, num);
     }
 
+    /**
+     * <code>isDouble</code> checks if a <code>String</code> is a decimal number
+     *
+     * @param in the <code>String</code> to be tested.
+     * @return <code>true</code> if the <code>String</code> is a decimal number, <code>false</code> otherwise.
+     */
     public static boolean isDouble(String in){
         try{
             Double.parseDouble(in);
