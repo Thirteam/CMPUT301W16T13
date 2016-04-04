1768c4a79c5ef0f22e80ac4b519f04358740f878
WeiLing5 <weiling588885@gmail.com>
2016-04-03 20:04:07 -0600

    Update Bid.java

--- TextbookHub/app/src/main/java/cmput301/textbookhub/Models/Bid.java
+++ TextbookHub/app/src/main/java/cmput301/textbookhub/Models/Bid.java
@@ -3,6 +3,13 @@ package cmput301.textbookhub.Models;
 import java.util.Calendar;
 
 /**
+ * <code>Bid</code> is use to return the bid amount, bidder, bid time made from <code>User</code> on a <code>Textbook</code>.
+ * 
+ * @author Thirteam
+ * @version 1.1
+ * @since 2016/03/08
+ * @see User
+ * @see Testbook
  * Created by Fred on 2016/3/8.
  */
 public class Bid implements Comparable, UniqueItem<String>{
@@ -12,6 +19,12 @@ public class Bid implements Comparable, UniqueItem<String>{
     private String bidder;
     private String timestamp;
 
+    /**
+     * Create a new <code>Bid</code> by the user.
+     * 
+     * @param amount the amount bidded
+     * @param user the user bid on the textbook
+     * */
     public Bid(Double amount, User user){
         this.amount = amount;
         this.bidder = user.getName();
@@ -19,15 +32,30 @@ public class Bid implements Comparable, UniqueItem<String>{
         this.id = user.getID() + "_BID_" + this.timestamp;
     }
 
+    /**
+     * Return the amount bidded.
+     * 
+     * @retrun the bid amount
+     */
     public Double getAmount() {
 
         return amount;
     }
 
+    /**
+     * Return the bidder on the textbook.
+     * 
+     * @return the textbook bidder
+     */
     public String getBidder() {
         return bidder;
     }
 
+    /**
+     * Return the bid time.
+     * 
+     * @retrun the timestamp of the bid
+     */
     public String getTimestamp() {
         return timestamp;
     }
